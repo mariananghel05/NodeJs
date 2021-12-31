c8052f5a9c6534bf3c366f686c742fb9fa2e8bde
unknown <mariananghel99@gmail.com>
2021-12-31 15:39:48 +0200

    nothing

---
+++ database/mysql.js
@@ -0,0 +1,14 @@
+const mysql = require('mysql');
+
+const con = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'root',
+    database: 'test'
+});
+
+
+module.exports = con;
+
+
+
